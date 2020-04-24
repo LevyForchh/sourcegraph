dc7f8fffa589e35d212483f99255e2be10ebbaa1
Eric Fritz <eric@eric-fritz.com>
2020-04-24 10:54:54 -0500

    Refactor bundle manager client.

--- internal/codeintel/bundles/client/bundle_manager_client.go
+++ internal/codeintel/bundles/client/bundle_manager_client.go
@@ -20,7 +20,11 @@ type BundleManagerClient interface {
 	// SendUpload transfers a raw LSIF upload to the bundle manager to be stored on disk.
 	SendUpload(ctx context.Context, bundleID int, r io.Reader) error
 
+	// GetUploads retrieves a raw LSIF upload from disk. The file is written to a file in the
+	// given directory with a random filename. The generated filename is returned on success.
 	GetUpload(ctx context.Context, bundleID int, dir string) (string, error)
+
+	// SendDB transfers a converted databse to the bundle manager to be stored on disk.
 	SendDB(ctx context.Context, bundleID int, r io.Reader) error
 }
 
@@ -44,91 +48,80 @@ func (c *bundleManagerClientImpl) BundleClient(bundleID int) BundleClient {
 
 // SendUpload transfers a raw LSIF upload to the bundle manager to be stored on disk.
 func (c *bundleManagerClientImpl) SendUpload(ctx context.Context, bundleID int, r io.Reader) error {
-	url, err := url.Parse(fmt.Sprintf("%s/uploads/%d", c.bundleManagerURL, bundleID))
+	body, err := c.request(ctx, "POST", fmt.Sprintf("uploads/%d", bundleID), r)
 	if err != nil {
 		return err
 	}
-
-	req, err := http.NewRequest("POST", url.String(), r)
-	if err != nil {
-		return err
-	}
-
-	// TODO - use context
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status %d", resp.StatusCode)
-	}
-
+	body.Close()
 	return nil
 }
 
-func (c *bundleManagerClientImpl) GetUpload(ctx context.Context, bundleID int, dir string) (string, error) {
-	url, err := url.Parse(fmt.Sprintf("%s/uploads/%d", c.bundleManagerURL, bundleID))
+// GetUploads retrieves a raw LSIF upload from disk. The file is written to a file in the
+// given directory with a random filename. The generated filename is returned on success.
+func (c *bundleManagerClientImpl) GetUpload(ctx context.Context, bundleID int, dir string) (_ string, err error) {
+	body, err := c.request(ctx, "GET", fmt.Sprintf("uploads/%d", bundleID), nil)
 	if err != nil {
 		return "", err
 	}
+	defer body.Close()
 
-	req, err := http.NewRequest("GET", url.String(), nil)
+	f, err := openRandomFile(dir)
 	if err != nil {
 		return "", err
+	}
+	defer func() {
+		if closeErr := f.Close(); err == nil {
+			err = closeErr
+		}
+	}()
+
+	if _, err := io.Copy(f, body); err != nil {
+		return "", err
+	}
+
+	return f.Name(), nil
+}
+
+// SendDB transfers a converted databse to the bundle manager to be stored on disk.
+func (c *bundleManagerClientImpl) SendDB(ctx context.Context, bundleID int, r io.Reader) error {
+	body, err := c.request(ctx, "POST", fmt.Sprintf("dbs/%d", bundleID), r)
+	if err != nil {
+		return err
+	}
+	body.Close()
+	return nil
+}
+
+func (c *bundleManagerClientImpl) request(ctx context.Context, method, path string, body io.Reader) (io.ReadCloser, error) {
+	url, err := url.Parse(fmt.Sprintf("%s/%s", c.bundleManagerURL, path))
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, url.String(), body)
+	if err != nil {
+		return nil, err
 	}
 
 	// TODO - use context
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("unexpected status %d", resp.StatusCode)
+		resp.Body.Close()
+		return nil, fmt.Errorf("unexpected status %d", resp.StatusCode)
 	}
 
+	return resp.Body, nil
+}
+
+func openRandomFile(dir string) (*os.File, error) {
 	uuid, err := uuid.NewRandom()
 	if err != nil {
-		return "", err
-	}
-	filename := filepath.Join(dir, uuid.String())
-
-	f, err := os.Create(filename)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	if _, err := io.Copy(f, resp.Body); err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return filename, nil
-}
-
-func (c *bundleManagerClientImpl) SendDB(ctx context.Context, bundleID int, r io.Reader) error {
-	url, err := url.Parse(fmt.Sprintf("%s/dbs/%d", c.bundleManagerURL, bundleID))
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", url.String(), r)
-	if err != nil {
-		return err
-	}
-
-	// TODO - use context
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	// TODO - safe to not close body here?
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status %d", resp.StatusCode)
-	}
-
-	return nil
+	return os.Create(filepath.Join(dir, uuid.String()))
 }
